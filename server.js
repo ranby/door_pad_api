f043585e7682ebc366ebb10f267c347d7223a78e
Erik Ranby <erik.ranby@gmail.com>
2016-05-24 19:37:52 +0200

    added support for cors

--- server.js
+++ server.js
@@ -7,13 +7,17 @@
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
+var cors 	   = require('cors');
+
+// allow CORS
+app.use(cors());
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080;        // set our port
+var port = process.env.PORT || 3000;        // set our port
 
 // ROUTES FOR OUR API
 // =============================================================================
