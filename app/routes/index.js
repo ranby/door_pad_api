c744f0884d596f685e7f70ff0cd7f628d242d546
Erik Ranby <erik.ranby@gmail.com>
2016-05-27 23:54:26 +0200

    Weather includes several hours ahead with wind strength

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,7 @@
 var weather = require('./weather');
 var commute = require('./commute');
 var appRouter = function(app) {
-	app.get('/weather/home/now', weather.homeNow);
+	app.get('/weather/home', weather.home);
 	app.get('/commute/kolonnvagen', commute.kolonnvagen);
 	app.get('/commute/solnastation', commute.solnastation);
 }
--- app/routes/weather.js
+++ app/routes/weather.js
@@ -1,7 +1,20 @@
 var request = require('request');
 var moment = require('moment');
 
-var homeNow = function(req, res) {
+var getWeatherDescription = function(num) {
+	if (num < 3) 
+		return 'sun';
+	else if (num < 5)
+		return 'cloud-sun';
+	else if (num < 8) 
+		return 'cloud';
+	else if (num === 11 || num === 15)
+		return 'snow';
+	else 
+		return 'rain';
+}
+
+var home = function(req, res) {
 	var smhiUrlHagalund = "http://opendata-download-metfcst.smhi.se/api/category/pmp2g/version/2/geotype/point/lon/18.01141/lat/59.36251/data.json";
 	request(smhiUrlHagalund, function(error, response, body) {
 		console.log('SMHI answered: ' + response.statusCode);
@@ -12,27 +25,47 @@ var homeNow = function(req, res) {
 		var timeIndex = currentHour - forecastStartHour;
 
 		var wsym = result.timeSeries[timeIndex].parameters[18].values[0];
-		var wtext = '';
-		if (wsym < 3) 
-			wtext = 'sun';
-		else if (wsym < 5)
-			wtext = 'cloud-sun';
-		else if (wsym < 8) 
-			wtext = 'cloud';
-		else if (wsym === 11 || wsym === 15)
-			wtext = 'snow'
-		else 
-			wtext = 'rain'
+		
 
 		res.json({
-			time: moment(result.timeSeries[timeIndex].validTime).format('YYYY-MM-DD H:mm'),
-			temp: result.timeSeries[timeIndex].parameters[1].values[0],
-			wind: result.timeSeries[timeIndex].parameters[4].values[0],
-			pic: wtext
+			weather: [
+			{
+				date: moment(result.timeSeries[timeIndex].validTime).format('YYYY-MM-DD'),
+				time: moment(result.timeSeries[timeIndex].validTime).format('H:mm'),
+				temp: result.timeSeries[timeIndex].parameters[1].values[0],
+				windStr: result.timeSeries[timeIndex].parameters[4].values[0],
+				windDir: result.timeSeries[timeIndex].parameters[3].values[0],
+				pic: getWeatherDescription(result.timeSeries[timeIndex].parameters[18].values[0])
+			},
+			{
+				date: moment(result.timeSeries[timeIndex+3].validTime).format('YYYY-MM-DD'),
+				time: moment(result.timeSeries[timeIndex+3].validTime).format('H:mm'),
+				temp: result.timeSeries[timeIndex+3].parameters[1].values[0],
+				windStr: result.timeSeries[timeIndex+3].parameters[4].values[0],
+				windDir: result.timeSeries[timeIndex+3].parameters[3].values[0],
+				pic: getWeatherDescription(result.timeSeries[timeIndex+3].parameters[18].values[0])
+			},
+			{
+				date: moment(result.timeSeries[timeIndex+6].validTime).format('YYYY-MM-DD'),
+				time: moment(result.timeSeries[timeIndex+6].validTime).format('H:mm'),
+				temp: result.timeSeries[timeIndex+6].parameters[1].values[0],
+				windStr: result.timeSeries[timeIndex+6].parameters[4].values[0],
+				windDir: result.timeSeries[timeIndex+6].parameters[3].values[0],
+				pic: getWeatherDescription(result.timeSeries[timeIndex+6].parameters[18].values[0])
+			},
+			{
+				date: moment(result.timeSeries[timeIndex+9].validTime).format('YYYY-MM-DD'),
+				time: moment(result.timeSeries[timeIndex+9].validTime).format('H:mm'),
+				temp: result.timeSeries[timeIndex+9].parameters[1].values[0],
+				windStr: result.timeSeries[timeIndex+9].parameters[4].values[0],
+				windDir: result.timeSeries[timeIndex+9].parameters[3].values[0],
+				pic: getWeatherDescription(result.timeSeries[timeIndex+9].parameters[18].values[0])
+			}
+			]
 		});
 	});
 }
 
 module.exports = {
-	homeNow : homeNow
+	home : home
 }
