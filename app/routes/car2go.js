cc946a1615750824cf24b2413f395ef249cf6b34
Erik Ranby <erik.ranby@gmail.com>
2016-05-31 22:09:43 +0200

    added car2go api route

---
+++ app/routes/car2go.js
@@ -0,0 +1,27 @@
+var request = require('request');
+
+var getCars = function(req, res) {
+	url = "http://www.car2go.com/api/v2.1/vehicles?loc=stockholm&oauth_consumer_key=DooR_Pad&format=json";
+	request(url, function(error, response, body) {
+		var result = JSON.parse(body);
+		var answer = {
+			cars: []
+		}
+		for (var i in result.placemarks) {
+			var item = result.placemarks[i];
+			answer.cars.push({
+				id: item.vin,
+				lng: item.coordinates[0],
+				lat: item.coordinates[1],
+				address: item.address,
+				fuel: item.fuel,
+				licensePlate: item.name,
+			});
+		}
+		res.json(answer);
+	});
+}
+
+module.exports = {
+	cars: getCars
+}
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,12 @@
 var weather = require('./weather');
 var commute = require('./commute');
+var car2go = require('./car2go');
+
 var appRouter = function(app) {
 	app.get('/weather/home', weather.home);
 	app.get('/commute/kolonnvagen', commute.kolonnvagen);
 	app.get('/commute/solnastation', commute.solnastation);
+	app.get('/car2go/cars', car2go.cars);
 }
 
 module.exports = appRouter;
