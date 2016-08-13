a8d0c69d05131e9e08c4669c1f14054f88304404
Erik Ranby <erik.ranby@gmail.com>
2016-08-13 18:13:02 +0200

    Removed old commute routes

--- app/routes/commute.js
+++ app/routes/commute.js
@@ -1,53 +1,6 @@
 var request = require('request');
 var secret = require('../../secrets');
-var stationIds = {};
-
-var rokubbsgatan = function(req, res) {
-	var slUrl = "http://api.sl.se/api2/realtimedepartures.json?key=" + secret.secret.sl.apiKey + "&siteid=1140&timewindow=60";
-	request(slUrl, function(error, response, body) {
-		console.log('SL (Rökubbsgatan) answered: ' + response.statusCode);
-
-		var result = JSON.parse(body);
-
-		var answer = {
-			departures: []
-		};
-		for (var i in result.ResponseData.Buses) {
-			var item = result.ResponseData.Buses[i];
-			var type = item.GroupOfLine == 'blåbuss' ? 'BLUE BUS' : 'BUS';
-			answer.departures.push({
-				line: item.LineNumber,
-				destination: item.Destination,
-				time: item.DisplayTime,
-				transportType: type
-			});
-		}
-		res.json(answer);
-	});
-}
-
-var gardet = function(req, res) {
-	var slUrl = "http://api.sl.se/api2/realtimedepartures.json?key=" + secret.secret.sl.apiKey + "&siteid=9221&timewindow=60";
-	request(slUrl, function(error, response, body) {
-		console.log('SL (Gärdet) answered: ' + response.statusCode);
-
-		var result = JSON.parse(body);
-
-		var answer = {
-			departures: []
-		};
-		for (var i in result.ResponseData.Metros) {
-			var item = result.ResponseData.Metros[i];
-			answer.departures.push({
-				line: item.LineNumber,
-				destination: item.Destination,
-				time: item.DisplayTime,
-				transportType: 'METRO'
-			});
-		}
-		res.json(answer);
-	});
-}
+var stationIds = {};	
 
 var retrieveDepartures = function(response, transportType) {
 	var answer = {
@@ -105,7 +58,5 @@ var station = function(req, res) {
 }
 
 module.exports = {
-	rokubbsgatan: rokubbsgatan,
-	gardet: gardet,
 	station: station
 }
--- app/routes/index.js
+++ app/routes/index.js
@@ -5,8 +5,6 @@ var drivenow = require('./drivenow.js');
 
 var appRouter = function(app) {
 	app.get('/weather/home', weather.home);
-	app.get('/commute/rokubbsgatan', commute.rokubbsgatan);
-	app.get('/commute/gardet', commute.gardet);
 	app.get('/car2go/cars', car2go.cars);
 	app.get('/drivenow/cars', drivenow.cars);
 	app.get('/commute/:station/:transportType', commute.station);
